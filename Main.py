a4fa14079162f29e2547ed866a177fe080457eeb
Temujin <temujinristic@gmail.com>
2026-03-25 14:20:02 +1100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,19 @@
+from curses.ascii import isupper
+
+password = input("Enter password: ")
+
+if len(password) < 8:
+    print("Password must be at least 8 characters long")
+    exit()
+else:
+    print("length ok")
+
+if
+
+#####################
+password.isUpper()
+password[3].isUpper()
+####################
+
+for letter in password:
+    if letter.isupper():
