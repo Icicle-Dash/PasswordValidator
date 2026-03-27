e5c0e612983159b454caa254b43ec9708bb2d621
Temujin <temujinristic@gmail.com>
2026-03-27 11:02:27 +1100

    Added 3 attempts section before the program exits

--- Main.py
+++ Main.py
@@ -1,42 +1,48 @@
-password = input("Enter password: ")
+for attempt in range(3):
+    password = input("Enter password: ")
 
-# Checks if the password has 8 characters
-if len(password) < 8:
-    print("Password must be at least 8 characters long")
-    exit()
-else:
-    print("Good length")
+    # Checks if the password has 8 characters
+    if len(password) < 8:
+        print("Password must be at least 8 characters long")
+        continue
+    else:
+        print("Good length")
 
-# Checks if the password has an uppercase letter
-valid = False
-for letter in password:
-    if letter.isupper():
-      valid = True
+    # Checks if the password has an uppercase letter
+    valid = False
+    for letter in password:
+        if letter.isupper():
+          valid = True
 
-if valid:
-    print("Password is valid")
-else:
-    print("Password is not valid")
+    if valid:
+        print("Password is valid")
+    else:
+        print("Password is not valid")
+        continue
 
-#Checks if the password has a lowercase letter
-valid1 = False
-for letter in password:
-    if letter.islower():
-      valid1 = True
+    #Checks if the password has a lowercase letter
+    valid1 = False
+    for letter in password:
+        if letter.islower():
+          valid1 = True
 
-if valid1:
-    print("Password is valid")
-else:
-    print("Password is not valid")
+    if valid1:
+        print("Password is valid")
+    else:
+        print("Password is not valid")
+        continue
 
-#Checks if the password has a number
-valid2 = False
-for letter in password:
-    if letter.isdigit():
-      valid2 = True
+    #Checks if the password has a number
+    valid2 = False
+    for letter in password:
+        if letter.isdigit():
+          valid2 = True
 
-if valid2:
-    print("Password is valid")
-else:
-    print("Password is not valid")
+    if valid2:
+        print("Password is valid")
+        print("Password meets all requirements!")
+        break
+    else:
+        print("Password is not valid")
+        continue
 
